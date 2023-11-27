d39c53b5e570e9ae1409b5a51a793d1b0b6408bf
khaled <152022156+khaledalmassri@users.noreply.github.com>
2023-11-27 20:34:06 +0200

    Add files via upload

---
+++ Data.dart
@@ -0,0 +1,18 @@
+List<Map<String, String>> students = [
+  {'id': '1', 'name': 'nawfal', 'gender': 'Male'},
+  {'id': '2', 'name': 'Jane', 'gender': 'Female'},
+  {'id': '3', 'name': 'saif', 'gender': 'Male'},
+  {'id': '4', 'name': 'ali', 'gender': 'Male'},
+  {'id': '4', 'name': 'bilal', 'gender': 'Male'},
+];
+
+List<String> events = ['Football','Tennis','BascketBall'];
+
+List<Map<String, String>> students1 = [
+  {'id': '1', 'name': 'nawfal', 'gender': 'Male', 'sport': 'Football'},
+  {'id': '2', 'name': 'Jane', 'gender': 'Female', 'sport': 'Basketball'},
+  {'id': '3', 'name': 'saif', 'gender': 'Male', 'sport': 'Football'},
+  {'id': '4', 'name': 'ali', 'gender': 'Male', 'sport': 'Basketball'},
+  {'id': '5', 'name': 'bilal', 'gender': 'Male', 'sport': 'running'},
+  {'id': '6', 'name': 'Nadim', 'gender': 'Male', 'sport': 'Tennis'},
+];
